8f9bf37ed179f7b3df6d0627db05df80a25d02d0
Purple_NightSky <117831432+PurpleNightsky@users.noreply.github.com>
2023-10-06 19:36:41 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {  
+    int N;
+    scanf("%d", &N);
+    printf("%d", N * (N - 1));
+    return 0;
+}
